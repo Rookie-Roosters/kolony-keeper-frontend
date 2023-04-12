cb6fb84e9f6b8fc7ccf29400cd09f898c15f913c
Lomlomm <vanessaserna60@gmail.com>
2023-04-12 08:15:35 -0600

    Delete unnecessary imports

--- lib/modules/dashboard/views/devices/device_create_view.dart
+++ lib/modules/dashboard/views/devices/device_create_view.dart
@@ -1,11 +1,7 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../global/components/components.dart';
-import '../../../../global/entities/entities.dart';
 import '../../../../global/models/models.dart';
 import '../../../../global/repositories/repositories.dart';
 import '../../../../global/usecases/devices/device_create_usecase.dart';
